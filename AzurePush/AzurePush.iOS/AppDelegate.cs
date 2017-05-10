27b5fb3c1865b3a18db0c833e369c5e2017da357
serdarsert <serdar@serdarsert.com>
2017-05-10 13:47:01 +0300

    code review,refactor..

--- AzurePush/AzurePush.iOS/AppDelegate.cs
+++ AzurePush/AzurePush.iOS/AppDelegate.cs
@@ -2,55 +2,94 @@
 using Newtonsoft.Json.Linq;
 using System;
 using UIKit;
+using WindowsAzure.Messaging;
 
 namespace AzurePush.iOS
 {
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private SBNotificationHub Hub { get; set; }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                       UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                       new NSSet());
 
-            var settings = UIUserNotificationSettings.GetSettingsForTypes(
-                 UIUserNotificationType.Alert
-                 | UIUserNotificationType.Badge
-                 | UIUserNotificationType.Sound,
-                 new NSSet());
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
 
-            UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
-            UIApplication.SharedApplication.RegisterForRemoteNotifications();
-
-            return base.FinishedLaunching(app, options);
+            return true;
         }
+
         public override void RegisteredForRemoteNotifications(UIApplication application,
      NSData deviceToken)
         {
-            const string templateBodyAPNS = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
+            Hub = new SBNotificationHub(Constants.ConnectionString, Constants.NotificationHubPath);
 
-            JObject templates = new JObject();
-            templates["genericMessage"] = new JObject
-             {
-               {"body", templateBodyAPNS}
-             };
+            Hub.UnregisterAllAsync(deviceToken, (error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Error calling Unregister: {0}", error.ToString());
+                    return;
+                }
 
-            Push push = TodoItemManager.DefaultManager.CurrentClient.GetPush();
-            push.RegisterAsync(deviceToken, templates);
+                NSSet tags = null; // create tags if you want
+                Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                        Console.WriteLine("RegisterNativeAsync error: " + errorCallback.ToString());
+                });
+            });
         }
+
         public override void DidReceiveRemoteNotification(UIApplication application,
     NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
         {
-            NSDictionary aps = userInfo.ObjectForKey(new NSString("aps")) as NSDictionary;
+            ProcessNotification(userInfo, false);
+        }
 
-            string alert = string.Empty;
-            if (aps.ContainsKey(new NSString("alert")))
-                alert = (aps[new NSString("alert")] as NSString).ToString();
-
-            if (!string.IsNullOrEmpty(alert))
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+            if (null != options && options.ContainsKey(new NSString("aps")))
             {
-                UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
-                avAlert.Show();
+                //Get the aps dictionary
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+                string alert = string.Empty;
+
+                //Extract the alert text
+                // NOTE: If you're using the simple alert by just specifying
+                // "  aps:{alert:"alert msg here"}  ", this will work fine.
+                // But if you're using a complex alert with Localization keys, etc.,
+                // your "alert" object from the aps dictionary will be another NSDictionary.
+                // Basically the JSON gets dumped right into a NSDictionary,
+                // so keep that in mind.
+                if (aps.ContainsKey(new NSString("alert")))
+                    alert = (aps[new NSString("alert")] as NSString).ToString();
+
+                //If this came from the ReceivedRemoteNotification while the app was running,
+                // we of course need to manually process things like the sound, badge, and alert.
+                if (!fromFinishedLaunching)
+                {
+                    //Manually show an alert
+                    if (!string.IsNullOrEmpty(alert))
+                    {
+                        UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+                        avAlert.Show();
+                    }
+                }
             }
         }
     }
---
+++ AzurePush/AzurePush.iOS/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AzurePush.iOS
+{
+    public class Constants
+    {
+        // Azure app-specific connection string and hub path
+        public const string ConnectionString = "<Azure connection string>";
+        public const string NotificationHubPath = "<Azure hub path>";
+    }
+}
